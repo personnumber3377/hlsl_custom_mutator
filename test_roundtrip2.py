4a59dd119c07a2bf6e0e8ccd3ca96e83f1baf64b
personnumber3377 <sontapaa.jokulainen@gmail.com>
2026-04-05 22:55:45 +0300

    Blah

--- test_roundtrip2.py
+++ test_roundtrip2.py
@@ -15,53 +15,62 @@ def run_dxc(bin_path, file_path):
     except subprocess.CalledProcessError as e:
         return False, e.output.decode(errors="ignore")
 
+def run_one(dxc, path, data, fn):
+    # 1. run original
+    ok1, err1 = run_dxc(dxc, path)
+
+    # 2. parse → unparse
+    try:
+        src = strip(data).decode("utf-8", errors="ignore")
+
+        import hlsl_parser, hlsl_unparser
+
+        tu = hlsl_parser.parse_to_tree(src)
+        out = hlsl_unparser.unparse_tu(tu)
+
+    except Exception as e:
+        print(f"[PARSE FAIL] {fn}: {e}")
+        return
+    else:
+        print(f"[PARSE SUCCESS] {fn}")
+    # 3. rebuild input
+    new_data = b"\x00"*HEADER_SIZE + out.encode() + b"\x00"
+
+    tmp = "/tmp/test_shader.bin"
+    with open(tmp, "wb") as f:
+        f.write(new_data)
+
+    # 4. run again
+    ok2, err2 = run_dxc(dxc, tmp)
+
+    if ok1 != ok2:
+        print(f"[MISMATCH] {fn}")
+        print("Before:", ok1)
+        print("After :", ok2)
+        print("----")
+        print(err2)
+        # break
+
 def main():
     if len(sys.argv) != 3:
-        print("usage: test_roundtrip.py <dxc_binary> <input_dir>")
+        print("usage: test_roundtrip.py <dxc_binary> <input_dir/input_file>")
         return
 
     dxc = sys.argv[1]
     indir = sys.argv[2]
 
-    for fn in os.listdir(indir):
-        path = os.path.join(indir, fn)
-
-        with open(path, "rb") as f:
+    if os.path.isfile(indir): # Specified a file?
+        with open(indir, "rb") as f:
             data = f.read()
+        run_one(dxc, indir, data, indir)
+    else:
+        for fn in os.listdir(indir):
+            path = os.path.join(indir, fn)
 
-        # 1. run original
-        ok1, err1 = run_dxc(dxc, path)
+            with open(path, "rb") as f:
+                data = f.read()
 
-        # 2. parse → unparse
-        try:
-            src = strip(data).decode("utf-8", errors="ignore")
-
-            import hlsl_parser, hlsl_unparser
-
-            tu = hlsl_parser.parse_to_tree(src)
-            out = hlsl_unparser.unparse_tu(tu)
-
-        except Exception as e:
-            print(f"[PARSE FAIL] {fn}: {e}")
-            continue
-
-        # 3. rebuild input
-        new_data = b"\x00"*HEADER_SIZE + out.encode() + b"\x00"
-
-        tmp = "/tmp/test_shader.bin"
-        with open(tmp, "wb") as f:
-            f.write(new_data)
-
-        # 4. run again
-        ok2, err2 = run_dxc(dxc, tmp)
-
-        if ok1 != ok2:
-            print(f"[MISMATCH] {fn}")
-            print("Before:", ok1)
-            print("After :", ok2)
-            print("----")
-            print(err2)
-            break
+            run_one(dxc, path, data, fn)
 
     print("[+] Done")
 
